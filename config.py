c5a8f719d0b047a3fb8e04ebc976e64f4b5e6d51
Gabriela Lima <gabriela.deabreulima@gmail.com>
2026-01-17 23:30:26 -0300

    feat: setup initial project structure and database models

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///E:/ProjetosPython/TestToDoList/database.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'c696800d0246603a1168916d8a63207c'
